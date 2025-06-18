39d68b8c6890ead4c94cc2137da3c1d35b82e67f
Shemmy <shemmynyirenda3@gmail.com>
2025-06-18 22:02:15 +0200

    Coffre website

---
+++ app.vue
@@ -0,0 +1,191 @@
+<script setup>
+const colorMode = useColorMode();
+const isDarkMode = computed(() => colorMode.value == "dark");
+
+const toggleTheme = () => {
+  colorMode.preference = isDarkMode.value ? "light" : "dark";
+};
+
+const menuItems = [
+  { icon: "lucide:github", name: "Github", path: "/" },
+  { icon: "lucide:message-circle", name: "Telegram", path: "/about" },
+];
+const tags = [
+  "#Android",
+  "#Native",
+  "#Material-You",
+  "#MD3",
+  "#Java",
+  "#XML",
+  "#Gradle",
+];
+
+const windowWidth = ref(0);
+
+function updateWidth() {
+  windowWidth.value = window.innerWidth;
+}
+
+onMounted(() => {
+  windowWidth.value = window.innerWidth;
+  window.addEventListener("resize", updateWidth);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", updateWidth);
+});
+</script>
+
+<template>
+  <div class="relative w-full min-h-dvh">
+    <!--header-->
+    <header
+      class="fixed left-0 top-0 right-0 flex items-center backdrop-blur-xl bg-colorSurface/10 z-50"
+    >
+      <div
+        class="w-full xl:max-w-7xl px-4 py-2 md:px-8 mx-auto flex items-center justify-between"
+      >
+        <NuxtLink
+          to="/"
+          class="text-xl md:text-2xl font-bold text-colorOnPrimaryContainer"
+          >Coffre</NuxtLink
+        >
+        {{ windowWidth }}
+        <nav
+          class="flex items-center gap-2 md:gap-4 text-colorOnPrimaryContainer"
+        >
+          <button
+            v-for="item in menuItems"
+            class="size-12 flex items-center justify-center outline-0 rounded-full transition-all duration-300 hover:bg-colorSecondaryContainer cursor-pointer"
+          >
+            <Icon :name="item.icon" size="24" class />
+          </button>
+          <button
+            @click.stop.prevent="toggleTheme"
+            aria-label="Switch Theme"
+            :name="isDarkMode ? 'lucide:sun' : 'lucide:moon'"
+            class="size-12 flex items-center justify-center outline-0 theme-toggle-button rounded-full transition-all duration-300 hover:bg-colorSecondaryContainer cursor-pointer"
+          >
+            <Icon class="pointer-events-none" name="lucide:sun" size="24" />
+          </button>
+        </nav>
+      </div>
+    </header>
+    <!--header-->
+
+    <!--main-->
+    <main
+      class="relative w-full min-h-dvh 2xl:min-h-[826px] xl:max-w-7xl px-4 py-2 pt-16 md:px-8 mx-auto flex max-lg:flex-col items-center md:gap-4"
+    >
+      <!-- Grid Background-->
+      <div
+        id="grid-background"
+        class="absolute -z-10 top-0 left-0 right-0 bottom-0 grid-background opacity-10 pointer-events-none"
+      />
+      <div
+        id="left-gradient"
+        class="absolute -z-[9] left-0 top-0 bottom-0 w-[20%] bg-gradient-to-r from-colorSurface to-transparent"
+      />
+      <div
+        id="top-gradient"
+        class="absolute -z-[9] left-0 top-0 right-0 h-[20%] bg-gradient-to-b from-colorSurface to-transparent"
+      />
+      <div
+        id="right-gradient"
+        class="absolute -z-[9] top-0 bottom-0 right-0 w-[20%] bg-gradient-to-l from-colorSurface to-transparent"
+      />
+      <div
+        id="bottom-gradient"
+        class="absolute -z-[9] left-0 right-0 bottom-0 h-[30%] bg-gradient-to-t from-colorSurface to-transparent"
+      />
+      <!-- Grid Background-->
+      <!--Hero Text -->
+      <div class="md:w-fit flex flex-col gap-2 max-sm:pt-8">
+        <h1
+          class="scroll-m-20 font-phudu text-colorPrimary text-6xl font-extrabold tracking-tight lg:text-8xl"
+        >
+          COFFRE
+        </h1>
+        <h2
+          class="scroll-m-20 font-phudu text-2xl lg:text-5xl font-bold tracking-tight transition-colors"
+        >
+          Personal Finance Made Easy
+        </h2>
+        <p class="leading-7 md:text-lg text-colorOutline">
+          Track your income, expenses, and transfers with ease, gain insights
+          into your spending habits, and achieve your financial goals.
+        </p>
+
+        <div class="mt-8 flex items-center gap-2 md:gap-4">
+          <NuxtLink to="/download">
+            <button
+              class="px-4 py-2 md:px-8 md:py-3 cursor-pointer drop-shadow-2xl shadow-2xl rounded-full hover:bg-colorOnPrimaryContainer hover:text-colorOnPrimary duration-300 transition-all bg-colorPrimary text-colorOnPrimary"
+            >
+              Download
+            </button>
+          </NuxtLink>
+          <NuxtLink to="/download">
+            <button
+              class="px-4 py-2 md:px-8 md:py-3 cursor-pointer drop-shadow-2xl shadow-2xl rounded-full bg-colorSecondaryContainer text-colorOnSecondaryContainer duration-300 transition-all hover:bg-colorSurfaceContainerLow"
+            >
+              Constribute
+            </button>
+          </NuxtLink>
+        </div>
+        <!--Tags-->
+        <div>
+          <div class="flex items-center gap-2 mt-8">
+            <span
+              v-for="(tag, index) in tags.slice(0, windowWidth <= 360 ? 3 : 4)"
+              class="text-nowrap text-xs md:text-sm px-3 py-1 transition-all duration-300 rounded-full hover:bg-colorSecondaryContainer border border-colorOutline text-colorOutline"
+            >
+              {{ tag }}</span
+            >
+          </div>
+          <div class="flex px-6 items-center gap-2 mt-2">
+            <span
+              v-for="(tag, index) in tags.slice(4, tags.length)"
+              class="text-nowrap text-sm px-3 py-1 transition-all duration-300 rounded-full hover:bg-colorSecondaryContainer border border-colorOutline text-colorOutline"
+            >
+              {{ tag }}</span
+            >
+          </div>
+        </div>
+      </div>
+      <!--Hero App -->
+      <div class="w-fit relative flex justify-center rounded-lg">
+        <Phone />
+      </div>
+      <div
+        id="bottom-gradient"
+        class="absolute left-0 right-0 bottom-0 h-[30%] bg-gradient-to-t from-colorSurface to-transparent"
+      />
+    </main>
+  </div>
+</template>
+<style>
+.theme-toggle-button {
+  transition: transform 0.5s ease, opacity 0.5s ease;
+}
+
+.theme-toggle-button:active {
+  transform: rotate(360deg);
+}
+
+.font-phudu {
+  font-family: "Phudu", sans-serif;
+}
+
+.grid-background {
+  background-image: repeating-linear-gradient(
+      var(--color-colorPrimary) 0 1px,
+      transparent 1px 100%
+    ),
+    repeating-linear-gradient(
+      90deg,
+      var(--color-colorPrimary) 0 1px,
+      transparent 1px 100%
+    );
+  background-size: 56px 56px;
+}
+</style>
